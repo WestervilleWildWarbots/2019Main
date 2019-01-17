e664ffd03589ac7d41cb0abf081374fefd8e1f4d
Robert Boyd III <rob@robertcboyd.com>
2019-01-17 18:36:33 -0500

    Fix spelling mistake

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,6 +4,6 @@ public class RobotMap {
   public static final int DRIVE_STICK = 0;
   public static final int XBOX_CONTROLLER = 1;
   public static final int FRONT_DISTANCE_SENSOR = 0;
-  public static final int REAR_DISTANCE_SNESOR = 1;
+  public static final int REAR_DISTANCE_SENSOR = 1;
   public static final int ANALOG_GYRO = 2;
 }
