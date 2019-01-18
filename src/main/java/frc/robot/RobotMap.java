730819d399d65a0b90855038d33a158a61fb12e1
nofva <mikepiek4902@gmail.com>
2019-01-17 20:18:21 -0500

    Drive command and subsystem that probably works

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,7 +1,17 @@
 package frc.robot;
 
 public class RobotMap {
+
+
+  /**
+   *
+   */
+
   public static final int DRIVE_STICK = 0;
   public static final int XBOX_CONTROLLER = 1;
 
+  public static final int MOTOR_FL = 2;
+  public static final int MOTOR_FR = 3;
+  public static final int MOTOR_BL = 4;
+  public static final int MOTOR_BR = 5;
 }
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,17 +1,29 @@
 package frc.robot.commands;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.OI.Axis;
+import frc.robot.Robot;
+import frc.robot.OI;
 
 public class DriveCommand extends Command {
+ public static double speed = 20;
   public DriveCommand() {
+     
   }
 
   @Override
   protected void initialize() {
+    speed = 20;
   }
 
   @Override
   protected void execute() {
+    speed = 20;
   }
 
   @Override
@@ -21,9 +33,11 @@ public class DriveCommand extends Command {
 
   @Override
   protected void end() {
+     speed = 0;
   }
 
   @Override
   protected void interrupted() {
+     speed = 0;
   }
 }
