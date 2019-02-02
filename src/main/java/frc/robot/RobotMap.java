468f69ee70f4a87b3d0e478c1a87d605728dc8be
MyQ-P <mikepiek4902@gmail.com>
2019-02-02 12:39:40 -0500

    map sensors,send data to network table andshuffleboard

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,9 +14,13 @@ public class RobotMap {
   //Posible drive methods
   public static final int DRIVE_STICK = 0;
   public static final int XBOX_CONTROLLER = 1;
+  
   public static final int FRONT_DISTANCE_SENSOR = 7;
   public static final int REAR_DISTANCE_SENSOR = 8;
-  public static final int LINE_TRACKER = 6;
+  public static final int LEFT_DISTANCE_SENSOR = 25;
+  public static final int RIGHT_DISTANCE_SENSOR = 26;
+  
+  public static final int LINE_TRACKER = 10;
   public static final int ANALOG_GYRO = 9;
 
   public static final int PNUM_GRABL = 12;
@@ -30,6 +34,15 @@ public class RobotMap {
   public static final int MOTOR_FR = 3;
   public static final int MOTOR_BL = 4;
   public static final int MOTOR_BR = 5;
+  public static final int MOTOR_LIFT = 6;
+
+  public static int ENCODER_FL = 20;
+  public static int ENCODER_FR = 21;
+  public static int ENCODER_BL = 22;
+  public static int ENCODER_BR = 23;
+  public static int ENCODER_LIFT = 24;
+
+
   //the default value to pass to getDouble() calls for NetworkTable
   public static final double DEFAULT_VALUE=0;
   
