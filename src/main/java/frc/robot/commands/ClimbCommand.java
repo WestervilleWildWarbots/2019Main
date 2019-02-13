6f782b2977d5d426edc2f975ee80e2ec8ebe0afe
MyQ-P <mikepiek4902@gmail.com>
2019-02-12 20:11:57 -0500

    develops method for climb with rough values

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -8,6 +8,8 @@ import frc.robot.subsystems.ClimbSubsystem;
 
 public class ClimbCommand extends Command {
   private ClimbSubsystem climb = Robot.climbSubsystem();
+  public static int climbTime = 0;
+  private boolean climbDone = false;
   public ClimbCommand() {
   }
 
@@ -17,20 +19,35 @@ public class ClimbCommand extends Command {
 
   @Override
   protected void execute() {
+    if(climbTime < 3001){
     climb.climbVert("up");
     climb.climbOut("out");
+    }else if(climbTime < 5001){
+      climb.climbOut("in");
+    }else if (climbTime < 8001){
+      climb.climbVert("down");
+    }else{
+      climb.climbVert("off");
+      climb.climbOut("off");
+      climbDone = true;
+    }
+    climbTime++;
   }
 
   @Override
   protected boolean isFinished() {
-    return false;
+    return climbDone;
   }
 
   @Override
   protected void end() {
+    climb.climbVert("off");
+    climb.climbOut("off");
   }
 
   @Override
   protected void interrupted() {
+    climb.climbVert("off");
+    climb.climbOut("off");
   }
 }
