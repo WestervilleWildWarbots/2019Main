1250241ec515c6b1152dea7ecab9c95f79471a05
MyQ-P <mikepiek4902@gmail.com>
2019-02-14 19:14:25 -0500

    Adds loggers in all commands. -MyQ

--- src/main/java/frc/robot/commands/LiftAdjustmentCommand.java
+++ src/main/java/frc/robot/commands/LiftAdjustmentCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Logger;
 import frc.robot.RobotMap;
 
 public class LiftAdjustmentCommand extends Command {
@@ -8,28 +9,33 @@ public class LiftAdjustmentCommand extends Command {
 	int move;
 	
     public LiftAdjustmentCommand(int move) {
-    	this.move = move;
+        this.move = move;
+        Logger.Log("Lift adjust constructed.");
     }
 
     protected void initialize() {
+        Logger.Log("Lift adjust initialized.");
     }
 
     protected void execute() {
-    	if(move < 0 || RobotMap.setPoint < 0) {
+        if(move < 0 || RobotMap.setPoint < 0) {
     		RobotMap.setPoint += move;
     	} else {
     		RobotMap.setPoint = 0;
     	}
-    	
+    	Logger.Log("Lift adjust executed.");
     }
 
     protected boolean isFinished() {
+        Logger.Log("Lift adjust finished.");
         return true;
     }
 
     protected void end() {
+        Logger.Log("Lift adjust ended.");
     }
 
     protected void interrupted() {
+        Logger.Log("Lift adjust interrupted.");
     }
 }
--- src/main/java/frc/robot/commands/LiftToTopCommand.java
+++ src/main/java/frc/robot/commands/LiftToTopCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Logger;
 import frc.robot.RobotMap;
 
 // Sets the arm to the very top
@@ -8,22 +9,28 @@ import frc.robot.RobotMap;
 public class LiftToTopCommand extends Command {
 
     public LiftToTopCommand() {
+        Logger.Log("Lift to top constructed.");
     }
 
     protected void initialize() {
+        Logger.Log("Lift to top initialized.");
     }
 
     protected void execute() {
-    	RobotMap.setPoint = -3600;
+        RobotMap.setPoint = -3600;
+        Logger.Log("Lift to top executed.");
     }
 
     protected boolean isFinished() {
+        Logger.Log("Lift to top finished.");
         return true;
     }
 
     protected void end() {
+        Logger.Log("Lift to top ended.");
     }
 
     protected void interrupted() {
+        Logger.Log("Lift to top interrupted.");
     }
 }
