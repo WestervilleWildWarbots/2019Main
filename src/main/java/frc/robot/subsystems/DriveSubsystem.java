646be3bd146ffa34a7887f0bbcc078191edd6085
MyQ-P <mikepiek4902@gmail.com>
2019-11-14 19:52:55 -0500

    start of DrivePID

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -3,8 +3,12 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import frc.robot.RobotMap;
 
 public class DriveSubsystem extends Subsystem {
@@ -12,6 +16,11 @@ public class DriveSubsystem extends Subsystem {
   private static WPI_TalonSRX frontRight;
   private static WPI_TalonSRX backLeft;
   private static WPI_TalonSRX backRight;
+  private static double pFL = 1;
+  private static double iFL = 1;
+  private static double dFL = 1;
+  public PIDSource inputFL;
+  private PIDOutput outputFL;
 
   private static AnalogGyro Gyro = new AnalogGyro(RobotMap.ANALOG_GYRO);
 
@@ -25,6 +34,11 @@ public class DriveSubsystem extends Subsystem {
 
     backLeft.follow(frontLeft);
     backRight.follow(frontRight);
+
+    inputFL = getEncoderFL();
+
+  new PIDController(pFL, iFL, dFL, inputFL, outputFL);
+
   }
 
   @Override
@@ -37,11 +51,26 @@ public class DriveSubsystem extends Subsystem {
       return Gyro.getAngle();
   }
 
+  public PIDSource getEncoderFL() {
+    return frontLeft.getSensorCollection().getQuadraturePosition();
+  }
+
+  public void getEncoderFR(){
+    frontRight.getSensorCollection().getQuadraturePosition();
+  }
+
+  public void getEncoderBL(){
+    backLeft.getSensorCollection().getQuadraturePosition();
+  }
+
+  public void getEncoderBR(){
+    backRight.getSensorCollection().getQuadraturePosition();
+  }
 
   public void drive(double leftSpeed, double rightSpeed) {
     frontLeft.set(leftSpeed);
     frontRight.set(rightSpeed);
   }
 
-  
+
 }
